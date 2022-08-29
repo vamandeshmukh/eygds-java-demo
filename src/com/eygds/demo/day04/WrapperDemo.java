17eb8825f5db7eaaf98c7383898960bee217df3a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-29 11:45:14 +0530

    type casting

---
+++ src/com/eygds/demo/day04/WrapperDemo.java
@@ -0,0 +1,21 @@
+package com.eygds.demo.day04;
+
+public class WrapperDemo {
+	
+	public static void main(String[] args) {
+		
+		int num = 10; // primitive type 
+		Integer num2 = 20; // object of Wrapper class 
+
+		Integer num3 = num; // boxing - autoboxing 
+		
+		int num4 = num3; // unboxing 
+		
+//		num.
+//		num2.
+//		Integer.
+		
+		
+	}
+
+}
