ee2f99eecd27cb7751dd95f5af5f56878e94be26
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-30 14:58:56 +0530

    exception

--- src/com/eygds/demo/day05/MtDemo.java
+++ src/com/eygds/demo/day05/MtDemo.java
@@ -4,17 +4,19 @@ public class MtDemo {
 
 	public static void main(String[] args) {
 
-//		Print print = new Print();
-//		print.printNums();
-//		Print print2 = new Print();
-//		print2.printNums();
+		Print print = new Print();
+		print.printNums();
+		Print print2 = new Print();
+		print2.printNums();
+		Print print3 = new Print();
+		print3.printNums();
 
-		Thread obj = new Thread(new Print());
-		obj.start();
-		Thread obj2 = new Thread(new Print());
-		obj2.start();
-		Thread obj3 = new Thread(new Print());
-		obj3.start();
+//		Thread obj = new Thread(new Print());
+//		obj.start();
+//		Thread obj2 = new Thread(new Print());
+//		obj2.start();
+//		Thread obj3 = new Thread(new Print());
+//		obj3.start();
 
 	}
 }
