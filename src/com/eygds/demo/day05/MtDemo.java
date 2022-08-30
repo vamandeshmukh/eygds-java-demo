e54006dadf82af0c34e1d51e0a028646448f6c98
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-30 15:11:17 +0530

    exception

--- src/com/eygds/demo/day05/MtDemo.java
+++ src/com/eygds/demo/day05/MtDemo.java
@@ -4,24 +4,53 @@ public class MtDemo {
 
 	public static void main(String[] args) {
 
-		Print print = new Print();
-		print.printNums();
-		Print print2 = new Print();
-		print2.printNums();
-		Print print3 = new Print();
-		print3.printNums();
+		System.out.println("Start");
 
-//		Thread obj = new Thread(new Print());
-//		obj.start();
-//		Thread obj2 = new Thread(new Print());
-//		obj2.start();
-//		Thread obj3 = new Thread(new Print());
-//		obj3.start();
+//		Print print = new Print();
+//		print.printNums();
+//		Print print2 = new Print();
+//		print2.printNums();
+//		Print print3 = new Print();
+//		print3.printNums();
+		
+//		Thread.		
+//		obj.
+		
+		Thread obj = new Thread(new Print());
+		obj.start();
+		Thread obj2 = new Thread(new Print());
+		obj2.start();
+		Thread obj3 = new Thread(new Print());
+		obj3.start();
+				
 
 	}
 }
 
-class Print extends Thread {
+// Mutlithreading with Thread class 
+
+//class Print extends Thread {
+//
+//	@Override
+//	public void run() {
+//		this.printNums();
+//	}
+//
+//	void printNums() {
+//		for (int i = 1; i <= 10; i++) {
+//			try {
+//				Thread.sleep(250);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			System.out.println(i);
+//		}
+//	}
+//}
+
+//Mutlithreading with Runnable interface 
+
+class Print implements Runnable {
 
 	@Override
 	public void run() {
@@ -38,5 +67,4 @@ class Print extends Thread {
 			System.out.println(i);
 		}
 	}
-
 }
