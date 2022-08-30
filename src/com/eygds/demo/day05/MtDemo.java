8fc1d64bb4c139490ecc8910ebeb7f7809734ed5
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-08-30 14:57:59 +0530

    multithreading

---
+++ src/com/eygds/demo/day05/MtDemo.java
@@ -0,0 +1,40 @@
+package com.eygds.demo.day05;
+
+public class MtDemo {
+
+	public static void main(String[] args) {
+
+//		Print print = new Print();
+//		print.printNums();
+//		Print print2 = new Print();
+//		print2.printNums();
+
+		Thread obj = new Thread(new Print());
+		obj.start();
+		Thread obj2 = new Thread(new Print());
+		obj2.start();
+		Thread obj3 = new Thread(new Print());
+		obj3.start();
+
+	}
+}
+
+class Print extends Thread {
+
+	@Override
+	public void run() {
+		this.printNums();
+	}
+
+	void printNums() {
+		for (int i = 1; i <= 10; i++) {
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+	}
+
+}
