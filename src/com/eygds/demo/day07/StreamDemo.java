f7cfb926c15efe647f7cce85d21993d15529ba18
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-02 12:41:46 +0530

    stream

--- src/com/eygds/demo/day07/StreamDemo.java
+++ src/com/eygds/demo/day07/StreamDemo.java
@@ -2,6 +2,8 @@ package com.eygds.demo.day07;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamDemo {
 
@@ -20,10 +22,55 @@ public class StreamDemo {
 			System.out.println(emp.toString());
 		}
 
-		System.out.println("empList with only salary > 90K with for each loop:");
+		System.out.println("empList with forEach method:");
+//		empList.forEach((e) -> {
+//			System.out.println(e.toString());
+//		});
+
+		empList.forEach(e -> System.out.println(e.toString()));
+
+		System.out.println("empList with salary > 90K with for each loop:");
 		for (Employee emp : empList) {
-			System.out.println(emp.toString());
+			if (emp.getSalary() > 90000)
+				System.out.println(emp.toString());
 		}
+
+		System.out.println("Convert collection to stream - with stream() method");
+		Stream<Employee> empStream = empList.stream();
+//		empStream.forEach(null);
+//		empStream.map(null);
+//		empStream.filter(null);
+//		empStream.skip(0);
+//		empStream.limit(0);
+//		empStream.collect(null);
+		empStream.forEach(e -> System.out.println(e.toString()));
+
+		System.out.println("Only first three employees from the empList:");
+		Stream<Employee> firstThreeEmps = empList.stream();
+		firstThreeEmps.limit(6).forEach(e -> System.out.println(e.toString()));
+
+		System.out.println("Only 3 to 4 employees from the empList:");
+		Stream<Employee> emps3to4 = empList.stream();
+		emps3to4.skip(2).limit(2).forEach(e -> System.out.println(e.toString()));
+
+		System.out.println("empList with salary > 90K :");
+
+		Stream<Employee> empsWithHighSalary = empList.stream();
+		empsWithHighSalary.filter(e -> e.getSalary() > 90000).forEach(e -> System.out.println(e.toString()));
+
+		System.out.println("Stream to list conversion with collect(Collectors.toList():");
+
+		Stream<Employee> empStr = empList.stream();
+		List<Employee> empLt = empStr.collect(Collectors.toList());
+		empLt.forEach(e -> System.out.println(e.toString()));
+
+		System.out.println("EmpList with salary < 90K using stream API");
+
+		List<Employee> empListWithLessSalary = empList.stream().filter(e -> e.getSalary() < 90000)
+				.collect(Collectors.toList()); // imp 
+
+		empListWithLessSalary.forEach(e -> System.out.println(e.toString()));
+
 	}
 
 }
